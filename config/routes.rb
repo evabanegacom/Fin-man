cda1a8aafec86299856f209d24eeedeabe1301c1
evabanegacom <udegbue69@gmail.com>
2024-02-11 08:57:12 +0100

    working on sign up

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,9 @@ Rails.application.routes.draw do
       post 'generate_activation_token', to: 'users#generate_activation_token'
       # User registration and activation routes
       get '/user/budgets/:user_id', to: 'budgets#userBudgets'
+
       resources :users, only: %i[create index]
+      
       get '/activate/:token', to: 'users#activate', as: 'activate_account'
       # post '/activate/:token', to: 'users#activate'
       post '/sign_in', to: 'users#sign_in'
